6c8730ad084fa8a044d7a5610310b98bc7c59a60
Afonso <afonso.m.paulo@gmail.com>
2026-04-25 11:59:35 +0100

    cenas

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,2 +1,2 @@
-echo "Rebuilding"
+Write-Output "Rebuilding"
 javac -encoding UTF-8 server/MacManager.java server/PasswordManager.java server/CriarUser.java server/MySaudeServer.java client/MySaude.java client/CryptoUtils.java client/KeyUtils.java
